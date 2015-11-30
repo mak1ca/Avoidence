a95ae749bf2916c75d252958c370260eb650a74e
BigRuss <russell.oberer@gmail.com>
2015-11-30 15:33:54 -0500

    intro
    Added An intro Screen

---
+++ Assets/Scripts/IntroChange.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IntroChange : MonoBehaviour 
+{
+	private float changeScreen = 0;
+
+	void Update()
+	{
+		changeScreen += Time.deltaTime;
+		Debug.Log(Mathf.Floor(changeScreen));
+		if (changeScreen >= 3) 
+		{
+			Debug.Log(changeScreen);
+			Application.LoadLevel("MainMenu");
+		}
+	}
+}
